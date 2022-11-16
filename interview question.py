f9172c0ffcec3710d75b176889e948a211c805e1
rahul <112940156+rahul5793@users.noreply.github.com>
2022-11-16 20:58:51 +0530

    Add files via upload

---
+++ interview question.py
@@ -0,0 +1,37 @@
+# quiz1= what is difference between list and tupe?
+# major diffrence list is mutable and tupple is immutable,
+#   my_list=[1,2,3,5]
+#   my_tupple=(5,6,8,90)
+# thats mean store list store element in square bracket and tupple is store element inround bracket.
+# Q.2 What is the pass statement in Python?
+# There may be times in our code when we haven’t decided what to do yet, but we must type something for it to be syntactically correct. In such a case, we use the pass statement.
+# def func(*args):
+# pass
+# Similarly, the break statement breaks out of a loop.
+# >>> for i in range(7):
+# if i==3: break
+# print(i)
+# 1
+# 2
+# Q.3 How do you get a list of all the keys in a dictionary?
+# Be specific in these type of Python Interview Questions and Answers.
+# For this, we use the function keys().
+# >>> mydict={'a':1,'b':2,'c':3,'e':5}
+# >>> mydict.keys()
+# Q4. What is a Negative Index in Python?
+# Negative numbers mean that you count from the right instead of the left. 
+# So, list[-1] refers to the last element, list[-2] is the second-last, and so on.
+# Q5.What is type conversion in Python?
+# Ans: Type conversion refers to the conversion of one data type into another.
+# int() – converts any data type into integer type
+# float() – converts any data type into float type
+# ord() – converts characters into integer
+# hex() – converts integers to hexadecimal
+# oct() – converts integer to octal
+# tuple() – This function is used to convert to a tuple.
+# set() – This function returns the type after converting to set.
+# list() – This function is used to convert any data type to a list type.
+# dict() – This function is used to convert a tuple of order (key, value) into a dictionary.
+# str() – Used to convert integer into a string.
+# complex(real,imag) – This function converts real numbers to complex(real,imag) number.
+
